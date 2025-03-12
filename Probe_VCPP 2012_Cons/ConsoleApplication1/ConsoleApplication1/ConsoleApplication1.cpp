07d068a651122c7e0d644ef250b809d59e295a88
Maksym Hrytsiuk <maxima.at.work@gmail.com>
2025-03-12 19:06:22 +0200

    Final commit

--- Probe_VCPP 2012_Cons/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Probe_VCPP 2012_Cons/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <windows.h>
 #include "price.h"
 
@@ -8,24 +9,43 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
 
     int totalHryvnia = 0, totalKopiykas = 0;
+    string inputMethod;
+    cout << "Enter 'file' to read from data.txt or 'console' to enter data manually: ";
+    cin >> inputMethod;
 
-    cout << "Enter data in the format: hryvnia kopiykas quantity (e.g.: 19 89 3)." << endl;
-    cout << "To finish, use an incorrect format." << endl;
+    ifstream inputFile;
+    bool useFile = (inputMethod == "file");
+    if (useFile) {
+        inputFile.open("data.txt");
+        if (!inputFile) {
+            cerr << "Error opening file. Switching to manual input." << endl;
+            useFile = false;
+        } else {
+            cout << "Reading data from data.txt..." << endl;
+        }
+    }
+
+    if (!useFile) {
+        cout << "Enter data in the format: hryvnia kopiykas quantity (for example: 19 89 3)." << endl;
+        cout << "To finish, use an incorrect format." << endl;
+    }
 
     int h, k, quantity;
-    while (cin >> h >> k >> quantity) {
+    while ((useFile ? inputFile : cin) >> h >> k >> quantity) {
         int itemHryvnia, itemKopiykas;
         multiplyPrice(h, k, quantity, itemHryvnia, itemKopiykas);
         addPrices(totalHryvnia, totalKopiykas, itemHryvnia, itemKopiykas, totalHryvnia, totalKopiykas);
     }
 
+    if (useFile) {
+        inputFile.close();
+    }
+
     int roundedHryvnia = totalHryvnia, roundedKopiykas = totalKopiykas;
     roundToNationalBank(roundedHryvnia, roundedKopiykas);
 
-    cout << "\nTotal bill amount: "
-         << totalHryvnia << " hryvnia " << totalKopiykas << " kopiykas" << endl;
-    cout << "Amount due (rounded): "
-         << roundedHryvnia << " hryvnia " << roundedKopiykas << " kopiykas" << endl;
+    cout << "\nTotal bill amount: " << totalHryvnia << " hryvnia " << totalKopiykas << " kopiykas" << endl;
+    cout << "Amount due (rounded): " << roundedHryvnia << " hryvnia " << roundedKopiykas << " kopiykas" << endl;
 
     return 0;
 }
